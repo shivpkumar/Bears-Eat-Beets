3a3a22586cd6dbbe03755986d32c203033ea26f2
Shiv Kumar <shiv@newrelic.com>
2014-01-13 19:54:57 -0800

    starting over

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Bearseatbeets::Application.config.session_store :cookie_store, key: '_bearseatbeets_session'
--- db/migrate/20140106035638_update_user_table.rb
+++
@@ -1,7 +0,0 @@
-class UpdateUserTable < ActiveRecord::Migration
-  def change
-    add_column :users, :name, :string
-    add_column :users, :team_name, :string
-    add_column :users, :points, :integer
-  end
-end
