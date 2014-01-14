8390d03937565796df3ea7caa4b7bd4a9eee4d35
Shiv Kumar <shiv@newrelic.com>
2014-01-13 21:39:32 -0800

    firebase demo works

---
+++ js/teams.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  var teamsRef = new Firebase('https://bearseatbeets.firebaseio.com/teams');
+
+  $(".team").click(function(){
+    $(this).toggleClass('selection');
+  });
+
+  $('#button').click(function() {
+    var $selection = $('.selection');
+
+    if ($selection.length == 1) {
+      teamsRef.set({name: $selection.attr('id')});
+    } else {
+      alert('No team selected.');
+    }
+  });
+
+  teamsRef.on('child_added', function(data) {
+    var $selected = $('#' + data.val());
+    $selected.addClass('selected');
+  });
+});
