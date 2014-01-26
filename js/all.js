80f179456a0851a3b0cadf603e765fc66868eb80
Shiv Kumar <shiv@newrelic.com>
2014-01-26 15:56:29 -0800

    cleaned up html, css and js

---
+++ js/all.js
@@ -0,0 +1,3 @@
+document.writeln("<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>");
+document.writeln("<script src='https://cdn.firebase.com/v0/firebase.js' type='text/javascript'></script>");
+document.writeln("<script src='js/draft.js' type='text/javascript'></script>");
